9ae2e0348019e2fc5a605b68d83800ce647035bb
Denver Sessink <dsessink@gmail.com>
2013-01-10 21:39:13 +0100

    README markdown markup

---
+++ Resources/spec/ui.js
@@ -0,0 +1,12 @@
+require("behave").andSetup(this);
+
+describe("Index screen", function() {
+    var index = null;
+    it("creates a index controller", function() {
+        index = Alloy.createController("index");
+        expect(index).notToBe(null);
+    });
+    it("creates a scrollableview for you and me and the rest of the world", function() {
+        expect(index.getView("scrollableViewID")).notToBe(undefined);
+    });
+});
